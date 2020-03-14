594da3bc186942189c162c544e72a0e7ad24e7c1
Blake Stanger <blakestanger@gmail.com>
2020-03-13 20:02:37 -0600

    I DID IT I'M DONE!!!!

--- src/Client.cpp
+++ src/Client.cpp
@@ -18,29 +18,198 @@
 #include "Client.hpp"
 
 Client::~Client() {
+
 }
 
+void Client::initialize(unsigned int player, unsigned int board_size) {
+    string make_player_to_string = to_string(player);
+    string nameofaDEMON = "player_" + make_player_to_string + ".action_board.json";
 
-void Client::initialize(unsigned int player, unsigned int board_size){
-}
+    if (player > 2) {throw ClientWrongPlayerNumberException();}
 
+    if(player < 1 ){throw ClientWrongPlayerNumberException();}
+
+
+
+        vector<int> ZELDA_IS_A_GREAT_GAME(board_size, 0);
+
+
+        vector<vector<int>> board(board_size, ZELDA_IS_A_GREAT_GAME);
+
+        ofstream file(nameofaDEMON);
+
+        {
+            cereal::JSONOutputArchive killer_queen(file);
+            killer_queen(CEREAL_NVP(board));
+        }
+        this->player = player;
+        this->board_size = board_size;
+
+        initialized = true;
+
+    }
 
 void Client::fire(unsigned int x, unsigned int y) {
+
+    if(x> board_size || y > board_size || x < 0 || y < 0) {
+        throw ClientException("HEXES AND VYES ARE OUT OF LINE");
+    }
+
+    string name = "player_";
+    name.append(to_string(player));
+    name.append(".shot.json");
+
+
+
+    /**
+       * Fires a shot on the coordinate target and creates a player_#.shot.json file
+       * @param x - coordinate
+       * @param y - coordinate
+       */
+
+
+
+    ofstream fire_file(name);
+    {
+        cereal::JSONOutputArchive write_archive(fire_file);
+        write_archive(CEREAL_NVP(x), CEREAL_NVP(y));
+
+
+    }
+
 }
 
 
 bool Client::result_available() {
+    /**
+   * Checks if a result file is available for
+   * @return true if result is available, false otherwise
+   */
+    string name = "player_";
+    name.append(to_string(player));
+    name.append(".result.json");
+
+    std::ifstream f;
+    f.open(name);
+    if (f.good()){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 
 int Client::get_result() {
+    /**
+ * Gets the result from the player_#.result.json
+ * @return the result as either HIT, MISS, or OUT_OF_BOUNDS
+ */
+
+    int HIT_or_MISS_or_OUTOFBOUNDS;
+
+    string name = "player_";
+    name.append(to_string(player));
+    name.append(".result.json");
+
+
+    ifstream result_ifp(name);
+
+    if (!result_ifp.good()){
+        throw ClientException("The result file was no good in get_result");
+    }
+
+    cereal::JSONInputArchive read_archive(result_ifp);
+    read_archive(HIT_or_MISS_or_OUTOFBOUNDS);
+
+    result_ifp.close();
+
+    remove(name.c_str());
+
+
+   // https://stackoverflow.com/questions/28162563/c-code-to-test-for-positive-negative-using-switch-and
+
+    switch(HIT_or_MISS_or_OUTOFBOUNDS) {
+
+        case 0:
+            return OUT_OF_BOUNDS;
+        case 1:
+            return HIT;
+        case-1:
+            return MISS;
+        default:
+            throw ClientException("problem with get_result");
+    }
+
+
 }
 
 
 
 void Client::update_action_board(int result, unsigned int x, unsigned int y) {
+
+
+    string number = to_string(player);
+    string fileName = "player_" + number + ".action_board.json";
+
+
+    ifstream file;
+    file.open(fileName);
+
+    if(!file){
+        throw ClientException("trouble opening player action board in update_action board");
+    }
+
+    vector<vector<int>> board(board_size, vector<int>(board_size));
+
+
+    cereal::JSONInputArchive jsonInputArchive(file);
+    jsonInputArchive(board);
+
+    board[y][x] = result;
+    // this is backwards wtf??? http://www.cplusplus.com/forum/beginner/68340/
+
+    file.close();
+
+    ofstream outf(fileName);
+
+    cereal::JSONOutputArchive headingOut(outf);
+    headingOut(CEREAL_NVP(board));
+
 }
 
 
 string Client::render_action_board(){
+
+   // char render_array[10][10];
+
+   //cant use char_array have to use vector/.
+
+
+    string name = "player_";
+    string action = ".action_board.json";
+    name.append(to_string(player));
+    string bardoon = name + action;
+
+    vector<vector<int>>Vitameatavegamin(board_size, vector<int>(board_size));
+
+    ifstream TOVALHALLA(bardoon);
+
+    cereal::JSONInputArchive THE_GREAT_LIBRARIAN(TOVALHALLA);
+
+    THE_GREAT_LIBRARIAN(Vitameatavegamin);
+
+string toret;
+string babycakes;
+
+
+    for(int i = 0; i<=this->board_size; i++){
+        for (int j = 0; j <= this->board_size; ++j) {
+           babycakes=  Vitameatavegamin[i][j];
+            toret.append(babycakes);
+
+        }
+    }
+
+    TOVALHALLA.close();
+    return toret;
 }
--- src/Server.cpp
+++ src/Server.cpp
@@ -26,19 +26,315 @@
  * @return length of the file in bytes
  */
 int get_file_length(ifstream *file){
+    std:: streampos fsize = 0;
+    fsize = file->tellg();
+    file->seekg( 0, std::ios::end);
+    fsize = file->tellg() - fsize;
+    file->close();
+    return fsize;
+    
+    // I never really used this. 
 }
 
 
 void Server::initialize(unsigned int board_size,
                         string p1_setup_board,
                         string p2_setup_board){
+
+//    /mnt/c/Users/blake/Documents/CSCI_366_Programming_Assignments/test/tests.cpp:65: Failure
+//    Expected: srv.initialize(10, "player_1.setup_board.txt", "player_2.setup_board.txt") doesn't throw an exception.
+//    Actual: it throws std::exception-derived exception with description: "Incorrect board size".
+//todo figure out what the F*** this means.
+
+    int file_length = 0;
+    int file_length2 = 0;
+    string line;
+    string different_line;
+
+
+   this->p1_setup_board.open(p1_setup_board);
+    if(!this->p1_setup_board.good()){
+        throw std::runtime_error("Unable to open P1 Board");
+    }
+
+    while(this->p1_setup_board>>line) {
+
+       // cout << line << line.length();
+
+        if (line.length() == board_size + 2 || line.length() == board_size) {
+
+            file_length++;
+
+
+        } else {
+            throw ServerException("PINKIncorrect board size");
+        }
+    }
+
+        if (file_length != board_size){
+
+            throw ServerException("GREENIncorrect board size");
+        }
+
+
+   //https://stackoverflow.com/questions/3482064/counting-the-number-of-lines-in-a-text-file
+    //conversation with cooper.
+    this->p1_setup_board.close();
+
+
+    this->p2_setup_board.open(p2_setup_board);
+    if(!this->p2_setup_board.good()){
+        throw std::runtime_error("Unable to open P2 Board");
+    }
+
+
+    while(this->p2_setup_board>>different_line) {
+
+        if (different_line.length() == board_size) {
+
+            file_length2++;
+
+
+        } else {
+            throw ServerException("REDIncorrect board size");
+        }
+    }
+        if (file_length2 != board_size){
+
+            throw ServerException("BLUEIncorrect board size");
+        }
+
+        //ok to assign board size now
+        this->board_size = board_size;
+    this->p2_setup_board.close();
+
 }
-
-
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+    /**
+    * Checks the coordinates of a shot against setup board of player
+    *
+    * Check that player number within bounds, checks that shot coordinates within bounds, determines if the shot
+    * results in a HIT, or a MISS.
+    * @param player - player number
+    * @param x - coordinate
+    * @param y - coordinate
+    * @return returns shot result as either HIT, MISS, or OUT_OF_BOUNDS
+    */
+
+
+
+    if (player < 1) {
+        throw invalid_argument("Player number not within bounds");
+    }
+
+    if (player > 2) {
+        throw invalid_argument("Player number not within bounds");
+    }
+
+
+    if ((y < 0) || (y > board_size)) {
+        return OUT_OF_BOUNDS;
+    }
+    if ((x < 0) || (x > board_size)) {
+        return OUT_OF_BOUNDS;
+    }
+
+
+
+    switch (player) {
+        case 2: {
+            string line_length;
+            this->p1_setup_board >> line_length;
+            int spot = (((line_length.length()-1 * y)) + x);
+            int i = 0;
+
+//            vector<vector<char>> STOP_BLOWING_HOLES_IN_MY_SHIP(board_size, vector<char>(board_size));
+//            for (int j = 0; j <STOP_BLOWING_HOLES_IN_MY_SHIP.size() ; ++j) {
+//                for (int k = 0; k <STOP_BLOWING_HOLES_IN_MY_SHIP[j].size() ; ++k) {
+//                    p1_setup_board>>line_length;
+//
+//                }
+//
+//            }
+            p1_setup_board.open("player_1.setup_board.txt");
+            p1_setup_board.seekg(spot, ios::beg);
+            char at_position[2];
+            p1_setup_board.read(at_position, 1);
+            at_position[1]=0;
+
+            p1_setup_board.close();
+
+            //cout<<at_position<<"ATPOSITIOS"<<endl;
+
+            while (i <= 4) {
+                //cout<<SHIPS[i]<<"SHIPSBITCHES"<<endl;
+                //cout<<at_position[0]<<"LOOKINGFORTHIS"<<endl;
+                if (SHIPS[i] == at_position[0]) {
+                    return HIT;
+                }
+                i++;
+            }
+            if(i==5){return MISS;}
+            break;
+        }
+        case 1: {
+            string HEAVY_METAL_BAND;
+            this->p2_setup_board.open("player_2.setup_board.txt");
+
+            if(!p2_setup_board.good()){throw ServerException("900");}
+            p2_setup_board >> HEAVY_METAL_BAND;
+            int spoter = (((HEAVY_METAL_BAND.length()-1) * y) + x);
+            int i = 0;
+
+
+
+            p2_setup_board.seekg(spoter, ios::beg);
+            char at_position2[2];
+
+            p2_setup_board.read(at_position2, 1);
+            at_position2[5];
+            //cout<<spoter<<"SPOT"<<x<<'X'<<y<<'y'<<at_position2<<"ATPOSITION"<<"LINELENGTH:"<<HEAVY_METAL_BAND.length()<<endl;
+            p2_setup_board.close();
+
+            while (i <= 4) {
+                if (SHIPS[i] == at_position2[0]) {
+                    return HIT;
+                }
+                i++;
+            }
+            if(i==5){return MISS;}
+
+        }
+        default:
+            cout << "HOW DID YOU REACH THIS?? YOU WERE NEVER SUPPOSED TO GET HERE> ";
+            break;
+
+
+    }
 }
 
 
 int Server::process_shot(unsigned int player) {
-   return NO_SHOT_FILE;
-}
+
+
+    int playercheck = player;
+    /**
+   * Processes a shot issued by player
+   *
+   * Gets the shot from player, extracts coordinates, passes the information of evaluate_shot, and writes the result
+   * into player_#.result.json.
+   * @param player - player number
+   * @return returns SHOT_PROCESSED, or NO_SHOT_FILE if nothing to process
+   */
+    bool inbounds = false;
+    bool xbound =false;
+    bool ybound = false;
+
+    string shooterMcGavin = (".shot.json");
+    string walrus = (to_string(player));
+    string name = "player_" + walrus + shooterMcGavin;
+
+
+
+    string return_name = "player_"+to_string(player)+".result.json";
+
+
+
+
+
+
+
+
+    if (playercheck < 1){
+        throw invalid_argument("Sorry this is a two player game!");
+    }
+
+    if (playercheck > 2){
+        throw invalid_argument("Sorry this is a two player game!");
+    }
+
+
+
+    ifstream shot_file;
+
+    shot_file.open(name);
+        if (!shot_file.good()){
+            //cout<<"WORLDROAMERXV-LT"<<player;
+
+            return NO_SHOT_FILE;
+        }
+
+
+        int x = 0, y = 0, result = 0;
+
+        //cout << x << "THIS IS X"<<endl;
+        //cout << y << "THIS IS Y"<<endl;
+        cereal::JSONInputArchive read_archive(shot_file);
+        read_archive(x, y);
+
+
+
+    shot_file.close();
+    remove(name.c_str());
+
+        if (x < board_size && x > -1) {xbound = true;}
+        if(y < board_size && y > -1)  {ybound = true;}
+        if(ybound&&xbound){inbounds = true; }
+//            cout << x << "NOW THIS IS X"<<endl;
+//            cout << y << "NOW THIS IS Y"<<endl;
+//            cout<< board_size << "THIS IS BOARD SIZE" <<endl;
+
+
+
+
+
+
+
+        if (!inbounds) {
+
+//            cout<<"TROUBLE";
+            playercheck = 3;
+
+        }
+
+//    if(inbounds){cout<<"INBOUNDS"<<playercheck<<"PLAYER"<<endl; }
+//    if(!inbounds){cout<<"OUTBOUNDS"<<playercheck<<"PLYAWER"<<endl;}
+
+
+        switch (playercheck) {
+            case 1:
+                //cout<<"CASE1";
+                result = evaluate_shot(playercheck, x, y);
+                //cout<<"CASE1"<<result<<endl;
+                break;
+            case 2:
+                result = evaluate_shot(playercheck, x, y);
+                //cout<<"CASE2"<<result<<endl;
+                break;
+            case 3:
+                result = OUT_OF_BOUNDS;
+                //cout << "DIDWEMAKEITHERE"<<endl;
+                break;
+            default:
+                throw ServerException("How did you get here this wasn't supposed to happen!");
+
+        }
+        //cout<<"tluser"<<result;
+
+        ofstream get_me_outta_here;
+        get_me_outta_here.open(return_name);
+        {
+            cereal::JSONOutputArchive king(get_me_outta_here);
+            king(CEREAL_NVP(result));
+        }
+
+
+        return SHOT_FILE_PROCESSED;
+    }
+
+
+
+
+
+
+
